f0a111916a6aa21e93d9360896e03535d060feeb
Sam <super8989@gmail.com>
2020-08-31 17:51:51 -0400

    Fix className

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -16,9 +16,9 @@ export default function DayListItem(props) {
 	});
 
 	return (
-		<li onClick={() => props.setDay(props.name)}>
-			<h2 className={dayClass}>{props.name}</h2>
-			<h3 className={dayClass}>{formatSpots(props.spots)} remaining</h3>
+		<li className={dayClass} onClick={() => props.setDay(props.name)}>
+			<h2>{props.name}</h2>
+			<h3>{formatSpots(props.spots)} remaining</h3>
 		</li>
 	);
 }
