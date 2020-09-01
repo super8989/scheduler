657a333dfcd259dffb1ee6c48cac4840beefe468
Sam <super8989@gmail.com>
2020-09-01 15:29:44 -0400

    Add Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf('Button', module)
 	.addParameters({
@@ -141,4 +142,12 @@ storiesOf('Appointment', module)
 	.add('Appointment', () => <Appointment />)
 	.add('Appointment with Time', () => <Appointment time={'12PM'} />)
 	.add('Header', () => <Header time={'12PM'} />)
-	.add('Empty', () => <Empty onAdd={action('onAdd')} />);
+	.add('Empty', () => <Empty onAdd={action('onAdd')} />)
+	.add('Show', () => (
+		<Show
+			student={'Lydia Miller-Jones'}
+			interviewer={interviewer}
+			onEdit={action('onEdit')}
+			onDelete={action('onDelete')}
+		/>
+	));
