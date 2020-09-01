1c48409ef01202cdb43e17569b2cf9611f5b098a
Sam <super8989@gmail.com>
2020-09-01 15:34:08 -0400

    Add Confirm component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from 'components/Button';
+
+function Confirm(props) {
+	const { message, onConfirm, onCancel } = props;
+
+	return (
+		<main className='appointment__card appointment__card--confirm'>
+			<h1 className='text--semi-bold'>{message}</h1>
+			<section className='appointment__actions'>
+				<Button danger onClick={onCancel}>
+					Cancel
+				</Button>
+				<Button danger onClick={onConfirm}>
+					Confirm
+				</Button>
+			</section>
+		</main>
+	);
+}
+
+export default Confirm;
