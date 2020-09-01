40ea3c1c14efffbd0bea2326f05cd78737ff6a49
Sam <super8989@gmail.com>
2020-09-01 00:23:51 -0400

    Building the Interviewer List

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import InterviewerListItem from 'components/InterviewerListItem';
+import 'components/InterviewerList.scss';
+
+function InterviewerList(props) {
+	const { interviewers } = props;
+
+	return (
+		<section className='interviewers'>
+			<h4 className='interviewers__header text--light'>Interviewer</h4>
+			<ul className='interviewers__list'>
+				{interviewers.map((person) => (
+					<InterviewerListItem
+						key={person.id}
+						avatar={person.avatar}
+						name={person.name}
+						setInterviewer={props.setInterviewer}
+						selected={props.interviewer === person.id}
+					/>
+				))}
+			</ul>
+		</section>
+	);
+}
+
+export default InterviewerList;
