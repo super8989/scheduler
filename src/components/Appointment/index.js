5326ed9654fd7cff2faa8b551a9afeff3783fd73
Sam <super8989@gmail.com>
2020-09-01 14:19:06 -0400

    Create the Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'components/Appointment/styles.scss';
+
+function Appointment() {
+	return <article className='appointment'>hello world</article>;
+}
+
+export default Appointment;
