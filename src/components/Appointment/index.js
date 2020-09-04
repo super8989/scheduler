d661fb13bc77dea436b2e7dd755d1925f4ceb77e
Sam <super8989@gmail.com>
2020-09-03 21:16:01 -0400

    Import useVisualMode hook to conditionally render Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,26 +3,24 @@ import React from 'react';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import useVisualMode from 'hooks/useVisualMode';
 
 import 'components/Appointment/styles.scss';
 
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+
 function Appointment(props) {
-	const { id, time, interview } = props;
+	const { id, time, interview, onAdd } = props;
+	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
 	return (
 		<article className='appointment'>
 			<Header time={time} />
-			{interview ? (
-				<Show
-					student={interview.student}
-					interviewer={interview.interviewer}
-					// onEdit
-					// onDelete
-				/>
-			) : (
-				<Empty
-				// onAdd
-				/>
+			{/* {interview ? (<Show student={interview.student} interviewer={interview.interviewer} onEdit onDelete />) : (<Empty onAdd/> )} */}
+			{mode === EMPTY && <Empty onAdd={onAdd} />}
+			{mode === SHOW && (
+				<Show student={interview.student} interviewer={interview.interviewer} />
 			)}
 		</article>
 	);
