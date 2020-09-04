873eabc46a92e1300447f7926afe755e2186186c
Sam <super8989@gmail.com>
2020-09-03 21:36:38 -0400

    Adding a Transition to onAdd props of Empty component when clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,16 +9,23 @@ import 'components/Appointment/styles.scss';
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 function Appointment(props) {
-	const { id, time, interview, onAdd } = props;
+	const { id, time, interview } = props;
 	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
 	return (
 		<article className='appointment'>
 			<Header time={time} />
 			{/* {interview ? (<Show student={interview.student} interviewer={interview.interviewer} onEdit onDelete />) : (<Empty onAdd/> )} */}
-			{mode === EMPTY && <Empty onAdd={onAdd} />}
+			{mode === EMPTY && (
+				<Empty
+					onAdd={() => {
+						transition(CREATE);
+					}}
+				/>
+			)}
 			{mode === SHOW && (
 				<Show student={interview.student} interviewer={interview.interviewer} />
 			)}
