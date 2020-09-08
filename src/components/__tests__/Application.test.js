94b212840a30b57a15c2c30fc5ad4eca4a5dbffe
Sam <super8989@gmail.com>
2020-09-07 22:58:13 -0400

    Test Application component with test data

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,25 @@
-import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
-import Application from "components/Application";
+import React from 'react';
+import {
+	render,
+	cleanup,
+	waitForElement,
+	fireEvent,
+	prettyDOM,
+} from '@testing-library/react';
+import Application from 'components/Application';
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it('defaults to Monday and changes the schedule when a new day is selected', () => {
+	const { getByText } = render(<Application />);
+
+	return waitForElement(() => getByText('Monday'))
+		.then(() => {
+			fireEvent.click(getByText('Tuesday'));
+			expect(getByText('Leopold Silvers')).toBeInTheDocument();
+		})
+		.then(() => {
+			fireEvent.click(getByText('Monday'));
+			expect(getByText('Archie Cohen')).toBeInTheDocument();
+		});
 });
